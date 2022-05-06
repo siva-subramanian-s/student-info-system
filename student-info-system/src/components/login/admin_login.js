92b6a806af390572e856d505f73302cf2e0e2bbf
gowtham <gowthamjpm534@gmail.com>
2022-05-06 20:53:45 +0530

    login

---
+++ student-info-system/src/components/login/admin_login.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./loginstyle_stud.css";
+
+function Login() {
+    return (
+      // student
+      <div className="Login">
+       <div class="login_user">
+    <div class="login_container">
+      <div class="wrapper">
+        <div class="title"><span>Admin Login</span></div>
+        <form action="#">
+          <div class="row">
+            <i class="fas fa-user"></i>
+            <input type="text" placeholder="Admin" minLength={7} required/>
+          </div>
+          <div class="row">
+            <i class="fas fa-lock"></i>
+            <input type="password" placeholder="Password" minlenght={8} required/>
+          </div>
+          
+          <div class="row button">
+            <input type="submit" value="Login"/>
+          </div>
+         
+        </form>
+      </div>
+    </div>
+  </div>
+  </div>
+
+
+     
+    );
+  }
+export default Login;
---
+++ student-info-system/src/components/login/staff_login.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./loginstyle_stud.css";
+
+function Login() {
+    return (
+      // student
+      <div className="Login">
+       <div class="login_user">
+    <div class="login_container">
+      <div class="wrapper">
+        <div class="title"><span>Staff Login</span></div>
+        <form action="#">
+          <div class="row">
+            <i class="fas fa-user"></i>
+            <input type="text" placeholder="Staff Name" minLength={7} required/>
+          </div>
+          <div class="row">
+            <i class="fas fa-lock"></i>
+            <input type="password" placeholder="Password" minLength={8} required/>
+          </div>
+          
+          <div class="row button">
+            <input type="submit" value="Login"/>
+          </div>
+         
+        </form>
+      </div>
+    </div>
+  </div>
+  </div>
+
+
+     
+    );
+  }
+export default Login;
